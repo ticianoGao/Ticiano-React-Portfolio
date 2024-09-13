f577ae1692653eb1965129b5b20cd7b0c68853a6
heracr0ss <ticianogao@outlook.com>
2024-09-13 03:09:05 -0400

    Update 09/13/2024
    Finished title and background

---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/playerStats.css";
+
+const PlayerStats = () => {
+  return (
+    <div id="playerStats">
+      <h1>TICIANO GAO LEVEL 24</h1>
+      <div id="playerStats__lines">
+        <span id="playerStats__lines__thick"></span>
+        <span id="playerStats__lines__thin"></span>
+      </div>
+      <h2>Computer Science Student</h2>
+    </div>
+  );
+};
+
+export default PlayerStats;
